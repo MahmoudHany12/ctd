2ff8dbc5495d3d420bc81f4e8dc2056d85696545
MahmoudHany12 <99086974+MahmoudHany12@users.noreply.github.com>
2022-04-07 23:49:46 +0200

    Add files via upload

---
+++ user interface.py
@@ -0,0 +1,34 @@
+import hashlib
+from tkinter import *
+import re
+password='SuperHardPassword99'
+PASHASH1 = hashlib.md5(password.encode("utf-8")).hexdigest()
+def fcomp():
+    e1=a1.get()
+    e2=a2.get()
+    PASHASH2 = hashlib.md5(e2.encode("utf-8")).hexdigest()
+    if e1 == "guest" and PASHASH2 == PASHASH1:
+        msg = Message(m, text = "you logged in").place(x=100,y= 100)
+    elif run(e1)==1 or run(e2)==1:
+        msg = Message(m, text = " don't enter special char ").place(x=100,y= 100)
+    elif e1=="" or e2=="":
+        msg = Message(m, text = " you didn't enter  ").place(x=100,y= 100)
+    else :  msg = Message(m, text = " wrong password or username ").place(x=100,y= 100)
+def run(string):
+	regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+	if(regex.search(string) == None):
+		return 0
+	else:
+		return 1
+m = Tk()
+m.geometry("900x300")
+m.title('                       LOGIN      ')
+Label(m, text='Username').grid(row=0)
+Label(m, text='password').grid(row=1)
+a1 = Entry(m)
+a2 = Entry(m)
+a1.grid(row=0, column=1)
+a2.grid(row=1, column=1)
+a2.config(show = '*')
+button = Button(m, text='login', width=20,fg ="blue",command=fcomp).place(x=240,y= 200)
+m.mainloop()
